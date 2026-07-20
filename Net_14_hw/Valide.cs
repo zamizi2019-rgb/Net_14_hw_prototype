17515a13868b10ea32648e9dee77378942d15a90
zamizi2019-rgb <zamizi2019@gmail.com>
2026-07-20 09:31:56 +0400

    Comments
    Updated comments in Valide.cs for clarity.

--- Net_14_hw/Valide.cs
+++ Net_14_hw/Valide.cs
@@ -1,4 +1,4 @@
-﻿public static class Valide
+﻿public static class Valide//кастомные проверки пароля и логина( без спец символов)
 {
     public static bool ValidUser(string login)
     {
@@ -26,11 +26,11 @@
 
     public static bool CheckBirthday(string date, out DateTime birthday)
     {
-        if (!DateTime.TryParse(date, out birthday))
+        if (!DateTime.TryParse(date, out birthday))//проверка на дата ли эьто
         {
             return false;
         }
-        if (birthday > DateTime.Now)
+        if (birthday > DateTime.Now)//провнерка на то что эта дата , до нынешней
         {
             return false;
         }
@@ -38,7 +38,7 @@
         return true;
     }
 
-    public static bool UserExists(string login)
+    public static bool UserExists(string login)//существует ли пользователь с таким логином уже
     {
         foreach (User user in GameData.Users)
         {
