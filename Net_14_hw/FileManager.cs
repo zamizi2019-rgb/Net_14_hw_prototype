533c5b078e30cd121849aea23940a4051964c8b5
zamizi2019-rgb <zamizi2019@gmail.com>
2026-07-20 09:23:47 +0400

    Comments

--- Net_14_hw/FileManager.cs
+++ Net_14_hw/FileManager.cs
@@ -2,7 +2,7 @@
 
 public static class FileManager
 {
-    public static void LoadUsers()
+    public static void LoadUsers()//загрузка юзера из файла
     {
         if (!File.Exists("Users.json"))
         {
@@ -18,12 +18,12 @@ public static class FileManager
             GameData.Users = new List<User>();
         }
     }
-    public static void SaveUsers()
+    public static void SaveUsers()//сохранение юзера
     {
         string json = JsonSerializer.Serialize(GameData.Users);
         File.WriteAllText("Users.json", json);
     }
-    public static void LoadQuizzes()
+    public static void LoadQuizzes()//она делает так что используя переменные мы можем обращаться к самому файлу и брать оттуда нужные нам строки и тд
     {
         GameData.HistoryQuiz = LoadQuiz("History.json");
         GameData.BiologyQuiz = LoadQuiz("Biology.json");
@@ -31,13 +31,13 @@ public static class FileManager
     }
     private static QuizData LoadQuiz(string path)
     {
-        if (!File.Exists(path))
+        if (!File.Exists(path))//проверка на существование файла
         {
             Console.WriteLine($"File {path} was not found!");
             return null;
         }
 
         string json = File.ReadAllText(path);
-        return JsonSerializer.Deserialize<QuizData>(json);
+        return JsonSerializer.Deserialize<QuizData>(json);//возвращает новый quizdata объект из джейсон строки
     }
 }
