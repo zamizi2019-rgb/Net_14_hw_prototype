13a9ecb14d0cccd791c09a61c7fd348455aefba3
zamizi2019-rgb <zamizi2019@gmail.com>
2026-07-21 12:12:07 +0400

    admin add

--- Net_14_hw/GameData.cs
+++ Net_14_hw/GameData.cs
@@ -1,6 +1,7 @@
 public static class GameData//хранилище данных
 {
     public static List<User> Users = new List<User>();//все зарег юзеры
+    public static List<Admin> Admins = new List<Admin>();//и админы
     public static QuizData HistoryQuiz;//все викторины
     public static QuizData BiologyQuiz;
     public static QuizData GeographyQuiz;
